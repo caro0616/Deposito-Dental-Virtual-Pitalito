12346181f99c3ee060af75e0e1205e906e6719ad
JuanPabloPadillCarvajal <jppadillac@eafit.edu.co>
2026-03-24 12:42:29 -0500

    feat(catalog): add public catalog service and controller (US-01)

---
+++ backend/src/modules/catalog/presentation/catalog.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { CatalogService } from '../application/catalog.service';
+
+@Controller('products')
+export class CatalogController {
+  constructor(private readonly catalogService: CatalogService) {}
+
+  @Get()
+  async listProducts(@Query('category') category?: string) {
+    if (category) {
+      return this.catalogService.getByCategory(category);
+    }
+    return this.catalogService.getPublicCatalog();
+  }
+
+  @Get(':id')
+  async getProduct(@Param('id') id: string) {
+    return this.catalogService.getProductById(id);
+  }
+}
